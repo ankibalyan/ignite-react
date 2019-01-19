249a938e88d1271641165c18407bcd3ed5f553cf
Ankit Kumar <ankit.kr.balyan@gmail.com>
2019-01-19 14:46:53 +0530

    added hmr, extract css minifier

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/index.scss';
-
-function App() {
-  return <div>Hello React</div>;
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept()
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+  mode: devMode ? 'development' : 'production',
   entry: {
     app: ['./src/index.js'],
   },
@@ -10,8 +15,12 @@ module.exports = {
     path: __dirname + "/build",
     pathinfo: false
   },
+  devtool: 'source-map',
   devServer: {
-    contentBase: './build'
+    contentBase: './build',
+    hot: true,
+    overlay: true,
+    open: true
   },
   resolve: {
     extensions: [".js", ".jsx", ".json"]
@@ -22,7 +31,19 @@ module.exports = {
       {
         test: /\.(css|scss|sass)$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          {
+            loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: ['file-loader']
       }
     ]
   },
@@ -30,6 +51,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
       file: 'index.html'
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.EnvironmentPlugin(["NODE_ENV"])
   ]
 };
